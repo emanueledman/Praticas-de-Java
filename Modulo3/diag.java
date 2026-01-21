3c5b825429059d6cbaeb3be9d948b207cfdff019
Emanuel Joao <emjoao@c2r2s10.42luanda.com>
2026-01-21 09:42:54 +0100

    Mat 1

--- Modulo3/diag.java
+++ Modulo3/diag.java
@@ -1,6 +1,25 @@
 package Modulo3;
 
 public class diag {
-    int [][]mat = new int[4][4];
-    mat
+    public static void main(String[] args) {
+        
+        int [][]mat = new int[4][4];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (i == 0 || j == 0) {
+                    mat[i][j] = 1;
+                }
+                else
+                    mat[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
 }
