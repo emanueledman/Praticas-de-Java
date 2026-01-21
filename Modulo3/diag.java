6f4bf336adc227edd3706f28fd5bc45ddeb22c11
Emanuel Joao <emjoao@c2r2s10.42luanda.com>
2026-01-21 09:52:25 +0100

    Mat 1

--- Modulo3/diag.java
+++ Modulo3/diag.java
@@ -18,7 +18,8 @@ public class diag {
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                System.out.print(mat[i][j] + " ");
+                if (i == j)
+                    System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
