010c08c9ea171c1427f4e16217958ef113d32583
Emanuel Joao <emjoao@c2r2s10.42luanda.com>
2026-01-21 15:12:36 +0100

    sistema de notas 1

---
+++ Exercicios/Biblioteca/Main.java
@@ -0,0 +1,21 @@
+package Exercicios.Biblioteca;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SISTEMA DE GESTAO DE BIBLIOTECA");
+        System.out.println("1 - Cadastrar livros\n" + //
+                        "2 - Cadastrar usuários\n" + //
+                        "3 - Realizar empréstimo (com validações: livro disponível, usuário não excedeu limite)\n" + //
+                        "4 - Realizar devolução\n" + //
+                        "5 - Buscar livro por ISBN\n" + //
+                        "6 - Listar livros disponíveis\n" + //
+                        "7 - Listar empréstimos de um usuário\n" + //
+                        "8 - Relatório completo da biblioteca\n" + //
+                        "9 - Calcular multas por atraso (R$2 por dia)\n" + //
+                        "10 - Persistir tudo em ArrayLists durante execução");
+    }
+}
+
+
